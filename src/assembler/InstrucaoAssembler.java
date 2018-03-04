1c550f46313d103c082ababed0d94a620303893e
pHgon <phenrikgoncalves@gmail.com>
2018-03-04 15:18:43 -0300

    Add OPs no InstrucaoAssembler

--- src/assembler/InstrucaoAssembler.java
+++ src/assembler/InstrucaoAssembler.java
@@ -16,14 +16,18 @@ public class InstrucaoAssembler {
     private String OP1;
     private String OP2;
     private String OP3;
+    private String OP4;
+    private String OP5;
     
     /**
      * 
      * @param l Label
      * @param Oper Operador
-     * @param OP1 Operando 1
-     * @param OP2 Operando 2
+     * @param OP1  Operando 1
+     * @param OP2  Operando 2
      * @param OP3  Operando 3
+     * @param OP4  Operando 4
+     * @param OP5  Operando 5
      */
     public InstrucaoAssembler(String l, String Oper, String OP1, String OP2, String OP3)
     {
@@ -32,6 +36,8 @@ public class InstrucaoAssembler {
         this.OP1 = OP1;
         this.OP2 = OP2;
         this.OP3 = OP3;
+        this.OP4 = OP4;
+        this.OP5 = OP5;
     }
     /**
      * getInstrucao
@@ -40,22 +46,31 @@ public class InstrucaoAssembler {
      */
     public  String getInstrucaoFULL()
     {   
-        if(getLabel() == "" && getOperator() != "" && getOP1() == "" && getOP2() == "" && getOP3() == "" )
+        if(getLabel() == "" && getOperator() != "" && getOP1() == "" && getOP2() == "" && getOP3() == "" && getOP4() == "" && getOP5() == "")
             return "\t" + getOperator();
-        if(getLabel() == "" && getOperator() != "" && getOP1() != "" && getOP2() == "" && getOP3() == "" )
+        if(getLabel() == "" && getOperator() != "" && getOP1() != "" && getOP2() == "" && getOP3() == "" && getOP4() == "" && getOP5() == "")
             return "\t" + getOperator() + "\t" + getOP1();
-        if(getLabel() == "" && getOperator() != "" && getOP1() != "" && getOP2() != "" && getOP3() == "" )
+        if(getLabel() == "" && getOperator() != "" && getOP1() != "" && getOP2() != "" && getOP3() == "" && getOP4() == "" && getOP5() == "")
             return "\t" + getOperator() + "\t" + getOP1() + "\t" + getOP2();
-        if(getLabel() == "" && getOperator() != "" && getOP1() != "" && getOP2() != "" && getOP3() != "" )
-            return "\t" + getOperator()+ "\t" + getOP1() + "\t" + getOP2() + "\t" + getOP3();        
-        if(getLabel() != "" && getOperator() != "" && getOP1() == "" && getOP2() == "" && getOP3() == "" )
+        if(getLabel() == "" && getOperator() != "" && getOP1() != "" && getOP2() != "" && getOP3() != "" && getOP4() == "" && getOP5() == "")
+            return "\t" + getOperator()+ "\t" + getOP1() + "\t" + getOP2() + "\t" + getOP3();
+        if(getLabel() == "" && getOperator() != "" && getOP1() != "" && getOP2() != "" && getOP3() != "" && getOP4() != "" && getOP5() == "")
+            return "\t" + getOperator()+ "\t" + getOP1() + "\t" + getOP2() + "\t" + getOP3() + "\t" + getOP4();   
+        if(getLabel() == "" && getOperator() != "" && getOP1() != "" && getOP2() != "" && getOP3() != "" && getOP4() != "" && getOP5() != "")
+            return "\t" + getOperator()+ "\t" + getOP1() + "\t" + getOP2() + "\t" + getOP3() + "\t" + getOP4() + "\t" + getOP5();   
+        
+        if(getLabel() != "" && getOperator() != "" && getOP1() == "" && getOP2() == "" && getOP3() == "" && getOP4() == "" && getOP5() == "")
             return getLabel() + "\t" + getOperator();
-        if(getLabel() != "" && getOperator() != "" && getOP1() != "" && getOP2() == "" && getOP3() == "" )
+        if(getLabel() != "" && getOperator() != "" && getOP1() != "" && getOP2() == "" && getOP3() == "" && getOP4() == "" && getOP5() == "")
             return getLabel() +"\t" + getOperator() + "\t" + getOP1();
-        if(getLabel() != "" && getOperator() != "" && getOP1() != "" && getOP2() != "" && getOP3() == "" )
+        if(getLabel() != "" && getOperator() != "" && getOP1() != "" && getOP2() != "" && getOP3() == "" && getOP4() == "" && getOP5() == "")
             return getLabel() +"\t" + getOperator() + "\t" + getOP1() + "\t" + getOP2();
-        if(getLabel() != "" && getOperator() != "" && getOP1() != "" && getOP2() != "" && getOP3() != "" )
+        if(getLabel() != "" && getOperator() != "" && getOP1() != "" && getOP2() != "" && getOP3() != "" && getOP4() == "" && getOP5() == "")
             return getLabel() +"\t" + getOperator()+ "\t" + getOP1() + "\t" + getOP2() + "\t" + getOP3();
+        if(getLabel() != "" && getOperator() != "" && getOP1() != "" && getOP2() != "" && getOP3() != "" && getOP4() != "" && getOP5() == "")
+            return getLabel() +"\t" + getOperator()+ "\t" + getOP1() + "\t" + getOP2() + "\t" + getOP3() + "\t" + getOP4();
+        if(getLabel() != "" && getOperator() != "" && getOP1() != "" && getOP2() != "" && getOP3() != "" && getOP4() != "" && getOP5() != "")
+            return getLabel() +"\t" + getOperator()+ "\t" + getOP1() + "\t" + getOP2() + "\t" + getOP3() + "\t" + getOP4() + "\t" + getOP5();
         
         return getLabel();
     }
@@ -128,6 +143,32 @@ public class InstrucaoAssembler {
      */
     public void setOP3(String OP3) {
         this.OP3 = OP3;
+    }
+    /**
+     * @return the OP4
+     */
+    public String getOP4() {
+        return OP4;
+    }
+
+    /**
+     * @param OP4 the OP4 to set
+     */
+    public void setOP4(String OP4) {
+        this.OP4 = OP4;
+    }
+    /**
+     * @return the OP5
+     */
+    public String getOP5() {
+        return OP5;
+    }
+
+    /**
+     * @param OP5 the OP5 to set
+     */
+    public void setOP5(String OP5) {
+        this.OP5 = OP5;
     }
     
 }
