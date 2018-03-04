d3f3e7ddd91ebc5f391c1cb58813d5a0923b2a2f
mtleme <mtleme@inf.ufpel.edu.br>
2018-03-04 20:04:52 -0300

    Segundo arquivo de texto

--- src/Run.java
+++ src/Run.java
@@ -28,18 +28,18 @@ public class Run {
         ProcessadorMacro pr1 = new ProcessadorMacro("/home/mateus/Área de Trabalho/semestre/ps/PSGIT/LC-3b_project/LC3EntradaProcMacro.txt");
         pr1.printExpanded();
         ArrayList<InstrucaoAssembler> entradaMontador1 = pr1.getEntradaMontador();
-        for(InstrucaoAssembler a : entradaMontador1)
-        {
-           System.out.println(a.getInstrucaoFULL());
-        }
+     //   for(InstrucaoAssembler a : entradaMontador1)
+      //  {
+      //     System.out.println(a.getInstrucaoFULL());
+       // }
         
         ProcessadorMacro pr2 = new ProcessadorMacro("/home/mateus/Área de Trabalho/semestre/ps/PSGIT/LC-3b_project/LC3EntradaProcMacro.txt");
         pr2.printExpanded();
         ArrayList<InstrucaoAssembler> entradaMontador2 = pr2.getEntradaMontador();
-        for(InstrucaoAssembler a : entradaMontador2)
-        {
-           System.out.println(a.getInstrucaoFULL());
-        }
+       // for(InstrucaoAssembler a : entradaMontador2)
+      //  {
+        //   System.out.println(a.getInstrucaoFULL());
+       // }
         
         
         /*Assembler ass1 = new Assembler(entradaMontador1);
